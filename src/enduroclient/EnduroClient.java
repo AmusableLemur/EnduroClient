2761790200bed9ffcb2ff0bf45fff31c840bd997
Rasmus Larsson <meister@rasmuslarsson.se>
2013-02-17 12:36:35 +0100

    Updated comments

--- src/enduroclient/EnduroClient.java
+++ src/enduroclient/EnduroClient.java
@@ -9,15 +9,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EnduroClient {
+
     private String serverURL;
 
     public EnduroClient(String serverURL) {
         this.serverURL = serverURL;
     }
 
+    /**
+     * Creates a connection to the server
+     *
+     * @param URL
+     * @param method
+     * @return
+     * @throws MalformedURLException
+     * @throws IOException
+     */
     private HttpURLConnection getConnection(String URL, String method) throws MalformedURLException, IOException {
         URL url = new URL(serverURL + URL);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         if (method.equals("POST") || method.equals("PUT")) {
             connection.setDoOutput(true);
@@ -30,6 +40,15 @@ public class EnduroClient {
         return connection;
     }
 
+    /**
+     * Reads all stored racer times from the server
+     *
+     * @param id The racer id (start number)
+     * @param label Should be "start" or "finish"
+     * @return ArrayList of all matching times
+     * @throws MalformedURLException
+     * @throws IOException
+     */
     public ArrayList<String> getRacerTimes(String id, String label) throws MalformedURLException, IOException {
         HttpURLConnection connection = getConnection("/" + label + "/" + id, "GET");
         ArrayList<String> response = new ArrayList<>();
@@ -45,6 +64,16 @@ public class EnduroClient {
         return response;
     }
 
+    /**
+     * Stores the specified racer time on the server, outputs the server
+     * response to the console
+     *
+     * @param id The racer id (start number)
+     * @param label Should be "start" or "finish"
+     * @param time Time formatted as "hh.mm.ss"
+     * @throws MalformedURLException
+     * @throws IOException
+     */
     public void storeRacerTime(String id, String label, String time) throws MalformedURLException, IOException {
         HttpURLConnection connection = getConnection("/" + label + "/" + id + "/" + time, "PUT");
 
